c11ba951ce90c8441b573f97a9836b3a894ebe14
Mark Hale <mark.hale@physics.org>
2023-01-01 21:18:45 +0000

    Added maximums.

--- bme280_service.py
+++ bme280_service.py
@@ -3,6 +3,14 @@ import bme280
 from service_utils import createService, SimpleService
 
 
+def _safe_min(newValue, currentValue):
+    return min(newValue, currentValue) if currentValue else newValue
+
+
+def _safe_max(newValue, currentValue):
+    return max(newValue, currentValue) if currentValue else newValue
+
+
 class BME280Service(SimpleService):
     def __init__(self, conn, i2cBus, i2cAddr):
         super().__init__(conn, i2cBus, i2cAddr, 'temperature', 'BME280')
@@ -16,10 +24,15 @@ class BME280Service(SimpleService):
         self.add_settable_path("/CustomName", "", "", "")
         self.service.add_path("/Pressure", None)
         self.service.add_path("/Humidity", None)
+        self.service.add_path("/History/MinimumTemperature", None)
+        self.service.add_path("/History/MaximumTemperature", None)
 
     def update(self):
         with SMBus(self.i2cBus) as bus:
             data = bme280.sample(bus, self.i2cAddr, self.calibrationParams)
-        self.service["/Temperature"] = round(data.temperature, 1)
+        temp = round(data.temperature, 1)
+        self.service["/Temperature"] = temp
         self.service["/Pressure"] = round(data.pressure, 1)
         self.service["/Humidity"] = round(data.humidity, 1)
+        self.service["/History/MinimumTemperature"] = _safe_min(temp, self.service["/History/MinimumTemperature"])
+        self.service["/History/MaximumTemperature"] = _safe_max(temp, self.service["/History/MaximumTemperature"])
--- ina219_service.py
+++ ina219_service.py
@@ -13,6 +13,12 @@ def toKWh(joules):
     return joules/3600/1000
 
 
+VOLTAGE_TEXT = lambda path,value: "{:.2f}V".format(value)
+CURRENT_TEXT = lambda path,value: "{:.3f}A".format(value)
+POWER_TEXT = lambda path,value: "{:.2f}W".format(value)
+ENERGY_TEXT = lambda path,value: "{:.6f}kWh".format(value)
+
+
 class INA219Service(SimpleService):
     def __init__(self, conn, i2cBus, i2cAddr, serviceType):
         super().__init__(conn, i2cBus, i2cAddr, serviceType, 'INA219')
@@ -21,29 +27,38 @@ class INA219Service(SimpleService):
         self.device = INA219(SHUNT_OHMS, busnum=self.i2cBus, address=self.i2cAddr, max_expected_amps=MAX_EXPECTED_AMPS)
         self.device.configure(voltage_range=INA219.RANGE_16V)
         self.device.sleep()
-        self.service.add_path("/Dc/0/Voltage", None, gettextcallback=lambda path,value: "{:.2f}V".format(value))
-        self.service.add_path("/Dc/0/Current", None, gettextcallback=lambda path,value: "{:.3f}A".format(value))
+        self.service.add_path("/Dc/0/Voltage", None, gettextcallback=VOLTAGE_TEXT)
+        self.service.add_path("/Dc/0/Current", None, gettextcallback=CURRENT_TEXT)
         self._configure_energy_history()
         self.service.add_path("/Alarms/LowVoltage", 0)
         self.service.add_path("/Alarms/HighVoltage", 0)
         self.service.add_path("/Alarms/LowTemperature", 0)
         self.service.add_path("/Alarms/HighTemperature", 0)
         self.add_settable_path("/CustomName", "", "", "")
-        self.service.add_path("/Dc/0/Power", None, gettextcallback=lambda path,value: "{:.2f}W".format(value))
+        self.service.add_path("/Dc/0/Power", None, gettextcallback=POWER_TEXT)
+        self.service.add_path("/History/MaximumVoltage", 0, gettextcallback=VOLTAGE_TEXT)
+        self.service.add_path("/History/MaximumCurrent", 0, gettextcallback=CURRENT_TEXT)
+        self.service.add_path("/History/MaximumPower", 0, gettextcallback=POWER_TEXT)
         self.lastPower = None
 
     def update(self):
         self.device.wake()
-        self.service["/Dc/0/Voltage"] = round(self._voltage(), 3)
-        self.service["/Dc/0/Current"] = round(self.device.current()/1000, 3)
-        power = self.device.power()/1000
+        voltage = round(self._voltage(), 3)
+        current = round(self.device.current()/1000, 3)
+        power = round(self.device.power()/1000, 3)
         now = time.perf_counter()
-        self.service["/Dc/0/Power"] = round(power, 3)
         self.device.sleep()
+
+        self.service["/Dc/0/Voltage"] = voltage
+        self.service["/Dc/0/Current"] = current
+        self.service["/Dc/0/Power"] = power
+        self.service["/History/MaximumVoltage"] = max(voltage, self.service["/History/MaximumVoltage"])
+        self.service["/History/MaximumCurrent"] = max(current, self.service["/History/MaximumCurrent"])
+        self.service["/History/MaximumPower"] = max(power, self.service["/History/MaximumPower"])
 
         if self.lastPower is not None:
             # trapezium integration
-            self._increment_energy_usage(round(toKWh((self.lastPower.power + power)/2 * (now - self.lastPower.timestamp)), 7))
+            self._increment_energy_usage(toKWh((self.lastPower.power + power)/2 * (now - self.lastPower.timestamp)))
         self.lastPower = PowerSample(power, now)
 
 
@@ -52,7 +67,7 @@ class INA219DCLoadService(INA219Service):
         super().__init__(conn, i2cBus, i2cAddr, 'dcload')
 
     def _configure_energy_history(self):
-        self.service.add_path('/History/EnergyIn', 0, gettextcallback=lambda path,value: "{:.6f}kWh".format(value))
+        self.service.add_path('/History/EnergyIn', 0, gettextcallback=ENERGY_TEXT)
 
     def _voltage(self):
         return self.device.supply_voltage()
@@ -66,7 +81,7 @@ class INA219DCSourceService(INA219Service):
         super().__init__(conn, i2cBus, i2cAddr, 'dcsource')
 
     def _configure_energy_history(self):
-        self.service.add_path('/History/EnergyOut', 0, gettextcallback=lambda path,value: "{:.6f}kWh".format(value))
+        self.service.add_path('/History/EnergyOut', 0, gettextcallback=ENERGY_TEXT)
 
     def _voltage(self):
         return self.device.voltage()

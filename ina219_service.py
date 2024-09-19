8ac376ef963b7762320342bc80ef7b1530a11eab
Mark Hale <mark.hale@physics.org>
2024-09-20 00:05:55 +0100

    Add invert current option.

--- ina219_service.py
+++ ina219_service.py
@@ -7,19 +7,20 @@ MAX_EXPECTED_AMPS = 2
 
 
 class INA219Service(DCI2CService):
-    def __init__(self, conn, i2cBus, i2cAddr, serviceType, maxExpectedCurrent=MAX_EXPECTED_AMPS, shuntResistance=SHUNT_OHMS):
-        super().__init__(conn, i2cBus, i2cAddr, serviceType, 'INA219', maxExpectedCurrent=maxExpectedCurrent, shuntResistance=shuntResistance)
+    def __init__(self, conn, i2cBus, i2cAddr, serviceType, maxExpectedCurrent=MAX_EXPECTED_AMPS, shuntResistance=SHUNT_OHMS, invertCurrent=False):
+        super().__init__(conn, i2cBus, i2cAddr, serviceType, 'INA219', maxExpectedCurrent=maxExpectedCurrent, shuntResistance=shuntResistance, invertCurrent=invertCurrent)
 
-    def _configure_service(self, maxExpectedCurrent, shuntResistance):
+    def _configure_service(self, maxExpectedCurrent, shuntResistance, invertCurrent):
         self.device = INA219(shuntResistance, busnum=self.i2cBus, address=self.i2cAddr, max_expected_amps=maxExpectedCurrent)
         self.device.configure(voltage_range=INA219.RANGE_16V)
         self.device.sleep()
+        self.current_direction = -1 if invertCurrent else 1
         super()._configure_service()
 
     def update(self):
         self.device.wake()
         voltage = round(self._voltage(), 3)
-        current = round(self.device.current()/1000, 3)
+        current = self.current_direction * round(self.device.current()/1000, 3)
         power = round(self.device.power()/1000, 3)
         now = time.perf_counter()  # record the time as close to measurement-taking as possible
         self.device.sleep()

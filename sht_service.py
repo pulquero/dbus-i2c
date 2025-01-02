5bd7762b4b63870f6a6ee0809aa3df199f88a67d
Mark Hale <mark.hale@physics.org>
2025-01-02 20:45:13 +0000

    Fix for new sht python packages.

--- sht_service.py
+++ sht_service.py
@@ -1,8 +1,10 @@
 from service_utils import SimpleI2CService
-from sensirion_i2c_driver.linux_i2c_transceiver import LinuxI2cTransceiver
-from sensirion_i2c_driver.connection import I2cConnection
-from sensirion_i2c_sht.sht3x import Sht3xI2cDevice
-
+from sensirion_driver_adapters.i2c_adapter.i2c_channel import I2cChannel
+from sensirion_driver_adapters.transfer import Transfer, execute_transfer
+from sensirion_driver_adapters.rx_tx_data import TxData, RxData
+from sensirion_i2c_driver import LinuxI2cTransceiver, I2cConnection, CrcCalculator
+from sensirion_i2c_sht3x import Sht3xDevice
+from sensirion_i2c_sht3x.commands import Repeatability
 
 class SHT3xService(SimpleI2CService):
     def __init__(self, conn, i2cBus, i2cAddr):
@@ -10,8 +12,8 @@ class SHT3xService(SimpleI2CService):
 
     def _configure_service(self):
         with LinuxI2cTransceiver(f"/dev/i2c-{self.i2cBus}") as bus:
-            device = Sht3xI2cDevice(I2cConnection(bus), self.i2cAddr)
-            serial_number = device.read_serial_number()
+            channel = I2cChannel(I2cConnection(bus), slave_address=self.i2cAddr, crc=CrcCalculator(8, 0x31, 0xff, 0x0))
+            serial_number = execute_transfer(channel, ReadSerialCommand())
         self.service.add_path("/Temperature", None)
         # default type is battery
         self.add_settable_path("/TemperatureType", 0, 0, 2)
@@ -20,7 +22,18 @@ class SHT3xService(SimpleI2CService):
 
     def update(self):
         with LinuxI2cTransceiver(f"/dev/i2c-{self.i2cBus}") as bus:
-            device = Sht3xI2cDevice(I2cConnection(bus), self.i2cAddr)
-            result = device.single_shot_measurement()
-        self.service["/Temperature"] = round(result[0].degrees_celsius, 1)
-        self.service["/Humidity"] = round(result[1].percent_rh, 1)
+            channel = I2cChannel(I2cConnection(bus), slave_address=self.i2cAddr, crc=CrcCalculator(8, 0x31, 0xff, 0x0))
+            device = Sht3xDevice(channel)
+            result = device.measure_single_shot(Repeatability.HIGH, True)
+        self.service["/Temperature"] = round(result[0].value, 1)
+        self.service["/Humidity"] = round(result[1].value, 1)
+
+
+class ReadSerialCommand(Transfer):
+    CMD_ID = 0x3780
+
+    def pack(self):
+        return self.tx_data.pack([])
+
+    tx = TxData(CMD_ID, '>H', device_busy_delay=0.01, slave_address=None, ignore_ack=False)
+    rx = RxData('>2H')

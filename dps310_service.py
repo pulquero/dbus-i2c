1aaa64bba0002e8caa61364a8481af0c2a60e01e
pulquero <mj_hale@yahoo.com>
2025-09-01 23:53:01 +0100

    Update dps310_service.py

--- dps310_service.py
+++ dps310_service.py
@@ -2,7 +2,6 @@ from smbus2 import SMBus
 import DPS
 from service_utils import SimpleI2CService
 
-dps310=DPS.DPS()
 
 def _safe_min(newValue, currentValue):
     return min(newValue, currentValue) if currentValue else newValue
@@ -14,11 +13,9 @@ def _safe_max(newValue, currentValue):
 
 class DPS310Service(SimpleI2CService):
     def __init__(self, conn, i2cBus, i2cAddr):
-        super().__init__(conn, i2cBus, i2cAddr, 'temperature', 'BME280')
+        super().__init__(conn, i2cBus, i2cAddr, 'temperature', 'DPS310')
 
     def _configure_service(self):
-#        with SMBus(self.i2cBus) as bus:
-#            self.calibrationParams = bme280.load_calibration_params(bus, self.i2cAddr)
         self.service.add_path("/Temperature", None)
         # default type is battery
         self.add_settable_path("/TemperatureType", 0, 0, 2)
@@ -28,6 +25,7 @@ class DPS310Service(SimpleI2CService):
 
     def update(self):
         with SMBus(self.i2cBus) as bus:
+            dps310 = DPS(bus, self.i2cAddr)
             scaled_p = dps310.calcScaledPressure()
             scaled_t = dps310.calcScaledTemperature()
             p = dps310.calcCompPressure(scaled_p, scaled_t)

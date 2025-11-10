caed0bd2a4be9a0bb3e2d4a10bdd5a3bc7e8c0e2
Mark Hale <mark.hale@physics.org>
2025-11-10 09:13:18 +0000

    Fixes #13. Add new temperature types.

--- bme280_service.py
+++ bme280_service.py
@@ -2,6 +2,14 @@ from smbus2 import SMBus
 import bme280
 from service_utils import SimpleI2CService
 
+BATTERY_TYPE_BATTERY = 0
+BATTERY_TYPE_FRIDGE = 1
+BATTERY_TYPE_GENERIC = 2
+BATTERY_TYPE_ROOM = 3
+BATTERY_TYPE_OUTDOOR = 4
+BATTERY_TYPE_WATER_HEATER = 5
+BATTERY_TYPE_FREEZER = 6
+
 
 def _safe_min(newValue, currentValue):
     return min(newValue, currentValue) if currentValue else newValue
@@ -20,7 +28,7 @@ class BME280Service(SimpleI2CService):
             self.calibrationParams = bme280.load_calibration_params(bus, self.i2cAddr)
         self.service.add_path("/Temperature", None)
         # default type is battery
-        self.add_settable_path("/TemperatureType", 0, 0, 2)
+        self.add_settable_path("/TemperatureType", BATTERY_TYPE_BATTERY, 0, 6)
         self.service.add_path("/Pressure", None)
         self.service.add_path("/Humidity", None)
         self.service.add_path("/History/MinimumTemperature", None)
